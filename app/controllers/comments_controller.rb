9cb6743be5e0ab3e262e64c3c97ad4e0862677ba
Minh Nguyen <ng.huyminh91@gmail.com>
2016-12-18 17:03:08 +0700

    Prework: Add comment section
    Signed-off-by: Minh Nguyen <ng.huyminh91@gmail.com>

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -17,6 +17,14 @@ class CommentsController < ApplicationController
     redirect_to @article
   end
 
+  def show
+    #@comments = Comment.where({:source_id => params[:source_id], :source_type => params[:source_type]})
+  end
+
+  def index
+    @comments = Comment.where({:source_id => params[:article_id], :source_type => "Article"})
+  end
+
   
   private
     def comment_params
